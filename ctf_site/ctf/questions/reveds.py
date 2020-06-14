c27fe50bb61f4017e6bde83e8d81b24ed91806a9
Barr Kirel <nacojerk@hotmail.com>
2020-06-14 11:00:27 +0300

    Added PNP

--- ctf_site/ctf/questions/reveds.py
+++ ctf_site/ctf/questions/reveds.py
@@ -3,6 +3,13 @@ from ..question import question
 
 @question("resistor_question.html", "Resistance", 100, "Reveds")
 def resistors(answer):
-    if answer == "hello":
+    if answer.lower() == "hello":
+        return 1
+    return 2
+
+
+@question("pnp_question.html", "PNP", 1000, "Computers")
+def resistors(answer):
+    if answer.lower() == "no":
         return 1
     return 2
